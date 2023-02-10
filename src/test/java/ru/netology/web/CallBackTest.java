ca11e5cff53cf1a91587fa9d439f5e84ba979f9e
Yana Vitkovskaya <vitkovskaya.yana@gmail.com>
2023-02-10 17:57:50 +0300

    Changed

--- src/test/java/ru/netology/web/CallBackTest.java
+++ src/test/java/ru/netology/web/CallBackTest.java
@@ -10,7 +10,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
